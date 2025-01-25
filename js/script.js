1b77254da6bc2b0b67f042338e06a76a210961bc
Nicole Fernandez Sanchez <n.fernandez17@unapec.edu.do>
2025-01-24 21:51:06 -0400

    movie and tv details

--- js/script.js
+++ js/script.js
@@ -76,6 +76,10 @@ async function displayMovieDetails(){
   const movieId = window.location.search.split('?')[1];
   
   const movie = await fetchAPPIData(`movie/${movieId}`);
+
+  //overlay for background image
+  displayBackgroundImage('movie', movie.backdrop_path);
+
   const div = document.createElement('div');
   div.innerHTML = `<div class="details-top">
           <div>
@@ -125,6 +129,87 @@ async function displayMovieDetails(){
           </div>
         </div>`
         document.querySelector('#movie-details').appendChild(div);
+}
+
+//display show details
+async function displayShowDetails(){
+  const showId = window.location.search.split('?')[1];
+  
+  const show = await fetchAPPIData(`tv/${showId}`);
+
+  //overlay for background image
+  displayBackgroundImage('show', show.backdrop_path);
+
+  const div = document.createElement('div');
+  div.innerHTML = `<div class="details-top">
+          <div>
+          ${
+            show.poster_path
+            ? `<img
+          src="https://image.tmdb.org/t/p/w500${show.poster_path}"
+          class="card-img-top"
+          alt="${show.name}"
+        />` : `<img
+          src="images/no-image.jpg"
+          class="card-img-top"
+          alt="${show.name}"
+        />`
+        }
+          </div>
+          <div>
+            <h2>${show.name}</h2>
+            <p>
+              <i class="fas fa-star text-primary"></i>
+              ${show.vote_average.toFixed(1)} / 10
+            </p>
+            <p class="text-muted">Last Air Date: ${show.last_air_date}</p>
+            <p>
+              ${show.overview}
+            </p>
+            <h5>Genres</h5>
+            <ul class="list-group">
+              ${show.genres.map(genre=>{
+                return `<li>${genre.name}</li>`}).join('')}              
+            </ul>
+            <a href="${show.homepage}" target="_blank" class="btn">Visit Show Homepage</a>
+          </div>
+        </div>
+        <div class="details-bottom">
+          <h2>Show Info</h2>
+          <ul>
+            <li><span class="text-secondary">Number of episodes: </span> ${show.number_of_episodes}</li>
+            <li><span class="text-secondary">Last Episode to Air: </span> ${show.last_episode_to_air.name}</li>
+            <li><span class="text-secondary">Status:</span> ${show.status}</li>
+          </ul>
+          <h4>Production Companies</h4>
+          <div class="list-group">${show.production_companies.map((company)=>{
+            return `<span>${company.name}</span>`
+          }).join(', ')}
+          </div>
+        </div>`
+        document.querySelector('#show-details').appendChild(div);
+}
+//display background on details page
+function displayBackgroundImage(type, backgroundPath){
+  const overlayDiv = document.createElement('div');
+  overlayDiv.style.backgroundImage = `url(https://image.tmdb.org/t/p/original/${backgroundPath})`;
+  overlayDiv.style.backgroundSize = 'cover';
+  overlayDiv.style.backgroundPosition = 'center';
+  overlayDiv.style.backgroundRepeat = 'no-repeat';
+  overlayDiv.style.height = '100vh';
+  overlayDiv.style.width = '100vw';
+  overlayDiv.style.position = 'absolute';
+  overlayDiv.style.top = '0';
+  overlayDiv.style.left = '0';
+  overlayDiv.style.zIndex = '-1';
+  overlayDiv.style.opacity = '0.1';
+
+  if(type === 'movie'){
+    document.querySelector('#movie-details').appendChild(overlayDiv);
+  }else{
+    document.querySelector('#show-details').appendChild(overlayDiv);
+
+  }
 }
 
 //fetch data from tmdb api
@@ -181,7 +266,7 @@ function init(){
             displayMovieDetails();
             break;
         case '/tv-details.html':
-            console.log('TV Details');
+            displayShowDetails();
             break;
         case '/search.html':
             console.log('Search');
