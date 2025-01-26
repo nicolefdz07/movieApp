f7811ce41b9bff2227627f9eb724a92b81b4da36
Nicole Fernandez Sanchez <n.fernandez17@unapec.edu.do>
2025-01-25 21:49:48 -0400

    added now playing

--- js/script.js
+++ js/script.js
@@ -212,6 +212,54 @@ function displayBackgroundImage(type, backgroundPath){
   }
 }
 
+//Display slider movies
+async function displaySlider(){
+  const {results} = await fetchAPPIData('movie/now_playing');
+
+  console.log(results);
+
+  results.forEach((movie)=>{
+    const div = document.createElement('div');
+    div.classList.add('swiper-slide');
+    div.innerHTML = `
+    
+            <a href="movie-details.html?id=${movie.id}">
+              <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.title}" />
+            </a>
+            <h4 class="swiper-rating">
+              <i class="fas fa-star text-secondary"></i> ${movie.vote_average} / 10
+            </h4>
+          `;
+          document.querySelector('.swiper-wrapper').appendChild(div);
+          initSwiper();
+
+  });
+}
+
+function initSwiper(){
+  const swiper = new Swiper('.swiper', {
+    slidesPerView: 1,
+    spaceBewtween: 30,
+    freeMode: true,
+    loop: true,
+    autoplay:{
+      delay: 4000,
+      disableOnInteraction: false
+    },
+    breakpoints: {
+      500: {
+        slidesPerView: 2,
+      },
+      700: {
+        slidesPerView: 3,
+      },
+      1200: {
+        slidesPerView: 4,
+      }
+    }
+  });
+}
+
 //fetch data from tmdb api
 async function fetchAPPIData(endpoint){
     const API_KEY = 'b6d50363806e0d1ec5acdd47b562262c';
@@ -257,6 +305,7 @@ function init(){
     switch(global.currentPage){
         case '/':
         case '/index.html':
+          displaySlider();
             displayPopularMovies();
             break;
         case '/shows.html':
